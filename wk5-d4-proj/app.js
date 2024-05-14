65211f8db2ed7083323a54c4e4a31eae3f56dbb2
yoooitsb <163598870+yoooitsb@users.noreply.github.com>
2024-05-14 11:32:02 -0600

    Add files via upload

---
+++ wk5-d4-proj/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    $('.navItem').css('color', 'white');
+
+    $('#nav').css('background-color', '#145');
+
+    $('ul li:even').css('background-color', '#ccc');
+
+    $('ul li:odd').css('background-color', '#aaa');
+
+    $('input').val('YourFirstName');
+});
